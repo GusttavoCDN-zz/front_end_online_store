4c286c1091a8e8d76a3a8877f35cd1d8868ab4af
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-05-01 09:47:40 -0300

    Updates

---
+++ precache-manifest.6c305de254d66a97a8e7d8fda315b9e3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0468409701a3ed7e235a06f279764110",
+    "url": "/front_end_online_store/index.html"
+  },
+  {
+    "revision": "07ce5999bf6f908ea462",
+    "url": "/front_end_online_store/static/css/main.112db89c.chunk.css"
+  },
+  {
+    "revision": "beb1077db005dd91d681",
+    "url": "/front_end_online_store/static/js/2.16a647cf.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/front_end_online_store/static/js/2.16a647cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ce5999bf6f908ea462",
+    "url": "/front_end_online_store/static/js/main.c27dd62d.chunk.js"
+  },
+  {
+    "revision": "bf2dd0968b7f074b60a1",
+    "url": "/front_end_online_store/static/js/runtime-main.c16d5a91.js"
+  },
+  {
+    "revision": "18cdcc1e32eae230436fb545ac656435",
+    "url": "/front_end_online_store/static/media/bank-card.18cdcc1e.svg"
+  },
+  {
+    "revision": "2dbea7bb09890b662788ee0f6d2637d1",
+    "url": "/front_end_online_store/static/media/cart.2dbea7bb.svg"
+  },
+  {
+    "revision": "cdedd077e1facaa51852e8e82075a465",
+    "url": "/front_end_online_store/static/media/empty-cart.cdedd077.svg"
+  },
+  {
+    "revision": "5db86616beaaac3383a22ae895b1ddb2",
+    "url": "/front_end_online_store/static/media/home.5db86616.svg"
+  },
+  {
+    "revision": "52310e5170d42b8a65ae32a6537015fa",
+    "url": "/front_end_online_store/static/media/mastercard.52310e51.svg"
+  },
+  {
+    "revision": "baa95d583f9e2f1d0d49405624aea990",
+    "url": "/front_end_online_store/static/media/star.baa95d58.svg"
+  },
+  {
+    "revision": "859d7240a108411880f46b4233f18259",
+    "url": "/front_end_online_store/static/media/star_colored.859d7240.svg"
+  },
+  {
+    "revision": "87ded0e75ad7c5da3b021fe2f4af76c0",
+    "url": "/front_end_online_store/static/media/visa.87ded0e7.svg"
+  }
+]);
